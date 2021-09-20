00be75a01b9cac21f625dd7344bf76a7a2433d36
Adewusi <oadewusi@softalliance.co.ng>
2021-09-20 11:45:35 +0100

    Adding Java Tasks to the new Repo

---
+++ JavaDataTypes.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class JavaDataTypes {
+    public static void main(String[] args) {
+
+        byte age = 126;
+
+        short money =32_76;
+
+        long TooMuchMoney = -2876554434l;
+
+        float area = 12345678.90f;
+
+        double biggerArea = 123456789.69;
+
+        char option = 'z';
+
+        boolean isAround = true;
+
+        System.out.println(age);
+        System.out.println(money);
+        System.out.println(TooMuchMoney);
+        System.out.println(biggerArea);
+        System.out.println(option);
+        System.out.println(isAround);
+    }
+}
---
+++ StringAndConcat.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class StringAndConcat {
+    public static void main(String[] args) {
+       /** String bird = "backingdog";
+        String adjective = " is beautiful.";
+
+        String newString = bird + adjective;
+
+        System.out.println(newString);
+
+        // string and primitive concat
+
+        String movieName = "matrix: ";
+        int parts = 2;
+
+        boolean isReleased = false;
+
+        System.out.println(""+parts + isReleased );
+
+        //.concat() method
+
+        System.out.println(movieName.concat(parts+""));
+
+        System.out.println(movieName + parts ); **/
+// string and primitive concat
+       int age = 30;
+        System.out.println(age);
+
+        String Agewise = "This is the age requested and the value is: ";
+
+        System.out.println(Agewise + age );
+
+        // //.concat() method
+
+        Agewise.concat(age+"");
+        System.out.println(Agewise.concat(age+""));
+
+    }
+}
