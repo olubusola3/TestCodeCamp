4e4dbbe8aa53c9e3d095709f959c2f4b1e10d538
Adewusi <oadewusi@softalliance.co.ng>
2021-09-21 14:47:08 +0100

    Adding Java Tasks 16 area of a Circle

---
+++ BasicArithmeticOps.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class BasicArithmeticOps {
+    public static void main(String[] args) {
+        // basic Arithmetic
+       // double age1 = 20;
+       // byte age2 = 30;
+
+      /**  System.out.println(age1 + age2);
+        System.out.println(age1 - age2);
+        System.out.println(age1 / age2);
+        System.out.println(age1 * age2);
+
+       // incremental and decremental ++ --
+
+        age1++;
+        age1--;
+        System.out.println(age1++);
+        System.out.println(age1--);**/
+
+        //compound assignment operators -= +=/+ *=
+       // age1 -= age2;
+       // age1 += age2;
+      //  age1 /= age2;
+      //age1 *= age2;
+      //  which means age1=20 - age2=30 i.e 20-30=10
+      //  System.out.println(age1);
+        double  r = 12.7;
+        double pie =3.142;
+        double  area= pie * (r*r);
+        System.out.println("******** Area of a Circle**********");
+        System.out.println(area +"metres");
+
+
+
+    }
+}
