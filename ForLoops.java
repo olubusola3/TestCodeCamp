2eca9c1f020e5b5261fcebf4eb5012a2685867b0
Adewusi <oadewusi@softalliance.co.ng>
2021-09-22 14:03:10 +0100

    For Loop Lesson Even and Odd numbers Task 21

---
+++ ForLoops.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class ForLoops {
+    public static void main(String[] args) {
+
+        int b = 10;
+       /** for even numbers between 1-10
+        for (int a = 0; a<b; a++){
+             if(a%2==0){
+                  System.out.println("These are the even numbers between 1 - 10: "+a);
+            }**/
+
+              //   //for odd numbers between 1-10
+              for (int a = 0; a<b; a++){
+               if (a%2!=0){
+                  System.out.println("These are the odd numbers between 1 - 10: "+a);
+
+              }
+          }
+    }
+}
