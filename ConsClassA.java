0932ae54f62bfad2b44ff80a9b19e2422537f213
Adewusi <oadewusi@softalliance.co.ng>
2021-09-24 18:00:18 +0100

    Java Constructors lesson Task 25

---
+++ ConsClassA.java
@@ -0,0 +1,21 @@
+package com.introToOOP;
+
+public class ConsClassA {
+    String drive;
+    int tyres;
+    long engine;
+
+    public ConsClassA(String Cdrive) {
+        drive = Cdrive;
+            }
+
+    public ConsClassA(String Cdrive,int Ctyres) {
+        tyres = Ctyres;
+          }
+
+    public ConsClassA(String Cdrive,int tyres,long Cengine) {
+        engine = Cengine;
+
+    }
+
+   }
---
+++ ConsClassB.java
@@ -0,0 +1,11 @@
+package com.introToOOP;
+
+public class ConsClassB {
+    public static void main(String[] args) {
+        ConsClassA carparts = new ConsClassA("wheels");
+        ConsClassA carExtra = new ConsClassA("wheels",5);
+        ConsClassA car = new ConsClassA("wheels",4,1234432l);
+        
+
+    }
+}
