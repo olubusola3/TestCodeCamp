2b331708e58dbb541bbd7c52b2d52b4cfc317c03
Adewusi <oadewusi@softalliance.co.ng>
2021-09-24 12:22:17 +0100

    For Java OOP Task 23

---
+++ ClassA.java
@@ -0,0 +1,11 @@
+package introToOOP;
+
+public class ClassA {
+   String name;
+    public void Accept(String myLessonName){
+        name = myLessonName;
+
+        System.out.println(name);
+
+    }
+}
---
+++ ClassB.java
@@ -0,0 +1,12 @@
+package introToOOP;
+
+public class ClassB {
+
+    public static void main(String[] args) {
+
+        ClassA course = new ClassA();
+        course.name = "Java lesson 22";
+        course.Accept(course.name);
+
+    }
+}
