aa410b2edb50968d7ea6bffbf431276d292e9cb9
Adewusi <oadewusi@softalliance.co.ng>
2021-09-28 19:06:53 +0100

    Keyword Class task for Final task 31

---
+++ KeyWordClassA.java
@@ -0,0 +1,16 @@
+package com.introToOOP;
+
+public class KeyWordClassA {
+    //create 3 variables of a class A, ball Size, ball Colour,
+    // ball Texture. and make it impossible to change the value
+    // the ball Size in any method in that class
+    public static void main(String[] args) {
+        final int ballsize  =10;
+       // ballsize = 12;
+        System.out.println("The ball size is fixed and cant be changed, the size is "+ballsize+" ");
+        String ballcolor;
+        String balltexture;
+    }
+
+
+}
